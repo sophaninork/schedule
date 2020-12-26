aa9ccbd0bbb6ef00ab8c0013d4c9db439b6a9d40
Sophanin <o-sophanin@beniten.com>
2020-12-27 00:05:15 +0700

    feature/login-form

---
+++ Schedule/Views/FormLoginPage.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Schedule.Views
+{
+    partial class FormLoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Panel panel1;
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.emailTextbox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.email_label = new System.Windows.Forms.Label();
+            this.login_title = new System.Windows.Forms.Label();
+            panel1 = new System.Windows.Forms.Panel();
+            panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.Color.White;
+            panel1.Controls.Add(this.passwordTextbox);
+            panel1.Controls.Add(this.label1);
+            panel1.Controls.Add(this.emailTextbox);
+            panel1.Controls.Add(this.loginButton);
+            panel1.Controls.Add(this.email_label);
+            panel1.Controls.Add(this.login_title);
+            panel1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            panel1.Location = new System.Drawing.Point(390, 260);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(500, 280);
+            panel1.TabIndex = 0;
+            panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextbox.Location = new System.Drawing.Point(25, 155);
+            this.passwordTextbox.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PasswordChar = '•';
+            this.passwordTextbox.Size = new System.Drawing.Size(450, 30);
+            this.passwordTextbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Password:";
+            // 
+            // emailTextbox
+            // 
+            this.emailTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextbox.ForeColor = System.Drawing.Color.Black;
+            this.emailTextbox.Location = new System.Drawing.Point(25, 86);
+            this.emailTextbox.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.emailTextbox.Multiline = true;
+            this.emailTextbox.Name = "emailTextbox";
+            this.emailTextbox.Size = new System.Drawing.Size(450, 30);
+            this.emailTextbox.TabIndex = 1;
+            this.emailTextbox.TextChanged += new System.EventHandler(this.emailTextbox_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(144)))), ((int)(((byte)(220)))));
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderSize = 3;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(360, 220);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(115, 40);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.login_button);
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_label.Location = new System.Drawing.Point(25, 59);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(66, 25);
+            this.email_label.TabIndex = 3;
+            this.email_label.Text = "Email:";
+            this.email_label.Click += new System.EventHandler(this.email_label_Click);
+            // 
+            // login_title
+            // 
+            this.login_title.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.login_title.AutoSize = true;
+            this.login_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_title.Location = new System.Drawing.Point(107, 17);
+            this.login_title.Name = "login_title";
+            this.login_title.Size = new System.Drawing.Size(307, 32);
+            this.login_title.TabIndex = 0;
+            this.login_title.Text = "Login to your account";
+            this.login_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.login_title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FormLoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(85)))), ((int)(((byte)(104)))));
+            this.ClientSize = new System.Drawing.Size(1262, 753);
+            this.Controls.Add(panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1280, 800);
+            this.Name = "FormLoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Schedule demo";
+            this.Load += new System.EventHandler(this.FormLoginPage_Load);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label login_title;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox emailTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Schedule/Views/FormLoginPage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Schedule.Views
+{
+    public partial class FormLoginPage : Form
+    {
+        public FormLoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLoginPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void email_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_button(object sender, EventArgs e)
+        {
+            FormTeacherPage teacherForm = new FormTeacherPage();
+            teacherForm.Show();
+            this.Hide();
+        }
+
+        private void emailTextbox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
